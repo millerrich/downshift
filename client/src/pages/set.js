84b53f4f4713d10d027e6868b53d383440b99034
Henry Parrish <henryhparrish@gmail.com>
2020-10-23 11:41:41 -0500

    Styled set.js and App.css

--- client/src/pages/set.js
+++ client/src/pages/set.js
@@ -1,18 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import TimeKeeper from 'react-timekeeper';
- 
-function setAlarm(){
-  const [time, setTime] = useState('12:34pm')
- 
-  return (
-    <div>
-      <TimeKeeper
-        time={time}
-        onChange={(data) => setTime(data.formatted12)}
-      />
-      <span>Time is {time}</span>
-    </div>
-  )
+import {CardGroup, Card, Nav } from 'react-bootstrap';
+import '../App.css'
+import Navbar from './Navbar';
+
+function setAlarm() {
+    const [time, setTime] = useState('12:34pm')
+
+    return (
+
+        <>
+
+            < Navbar />
+            <CardGroup>
+                <Card className="homeCard">
+                    <div className="mt-5" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
+                        <Card className="clock">
+                            <TimeKeeper
+                                time={time}
+                                onChange={(data) => setTime(data.formatted12)}
+                            />
+                            <Card.Body>
+                                <Card.Text className="text-center">
+                                    <span className="timeSet">Selected Time: {time}</span>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Card>
+            </CardGroup>
+
+        </>
+    )
 }
 
 export default setAlarm;
